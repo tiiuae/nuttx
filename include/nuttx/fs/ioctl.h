2f23163c0a1bff5c142abcc74961db8b5443a4bd
Jukka Laitinen <jukkax@ssrc.tii.ae>
2022-04-06 14:07:08 +0400

    fs/vfs/fs_truncate.c: Use ioctl to truncate on non-mountpoint inode
    Signed-off-by: Jukka Laitinen <jukkax@ssrc.tii.ae>

--- include/nuttx/fs/ioctl.h
+++ include/nuttx/fs/ioctl.h
@@ -185,6 +185,9 @@
 #define FIOC_TRUNCATE   _FIOC(0x0010)     /* IN:  Length of the file after truncate
                                            * OUT: None
                                            */
+#define FIOC_TRUNCATE   _FIOC(0x0010)     /* IN:  Length of the file after truncate
+                                           * OUT: None
+                                           */
 
 /* NuttX file system ioctl definitions **************************************/
 
