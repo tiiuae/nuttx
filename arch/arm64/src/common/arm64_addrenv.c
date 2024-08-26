58433939a272dc7842bd4b7719a5a464f01d9009
Ville Juven <ville.juven@unikie.com>
2024-08-26 16:23:22 +0300

    [REVERTME] arm64_addrenc.c: Add more heap, if TLS_ALIGNED is set
    The TLS alignment requires more room in the stack, which means more
    _initial_ heap is required to accomodate the stack.
    
    Why 2x TLS_MAXSTACK ? No idea. This is a temporary fix, like the +1 page
    extra above.

--- arch/arm64/src/common/arm64_addrenv.c
+++ arch/arm64/src/common/arm64_addrenv.c
@@ -67,6 +67,7 @@
 #include <nuttx/compiler.h>
 #include <nuttx/irq.h>
 #include <nuttx/pgalloc.h>
+#include <nuttx/tls.h>
 
 #include <arch/barriers.h>
 
@@ -453,7 +454,13 @@ int up_addrenv_create(size_t textsize, size_t datasize, size_t heapsize,
 
   /* Allocate 1 extra page for heap, temporary fix for #5811 */
 
-  heapsize = heapsize + MM_PGALIGNUP(4*CONFIG_DEFAULT_TASK_STACKSIZE);
+  heapsize = heapsize + MM_PGALIGNUP(CONFIG_DEFAULT_TASK_STACKSIZE);
+
+#ifdef CONFIG_TLS_ALIGNED
+  /* Need more stack for TLS alignment */
+
+  heapsize += MM_PGALIGNUP(2 * TLS_MAXSTACK);
+#endif
 
   /* Map the reserved area */
 
