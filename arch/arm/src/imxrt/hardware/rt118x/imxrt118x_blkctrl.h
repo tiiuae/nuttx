677b6fe54639fcf91b834eccb619f71ca923f2fc
Jukka Laitinen <jukka.laitinen@tii.ae>
2026-09-22 11:59:04 +0300

    [UPSTREAM] arch/imxrt/imxrt118x_blkctrl.h: Add bit definitions for SRC.SRMASK
    Extract bitfield definitions for reset control from the iMXRT1180 RM
    
    Signed-off-by: Jukka Laitinen <jukka.laitinen@tii.ae>

--- arch/arm/src/imxrt/hardware/rt118x/imxrt118x_blkctrl.h
+++ arch/arm/src/imxrt/hardware/rt118x/imxrt118x_blkctrl.h
@@ -95,6 +95,38 @@
 #define IMXRT_SRC_SRSR_BBSM             (IMXRT_SRC_BASE + 0x04c)
 #define IMXRT_SRC_SRSR                  (IMXRT_SRC_BASE + 0x050)  /* Reset Status */
 
+/* SRC.SRMASK bit fields */
+
+#define SRC_SRMASK_WDOG1_MASK           (1u << 0)
+#define SRC_SRMASK_WDOG2_MASK           (1u << 1)
+#define SRC_SRMASK_WDOG3_MASK           (1u << 2)
+#define SRC_SRMASK_WDOG4_MASK           (1u << 3)
+#define SRC_SRMASK_WDOG5_MASK           (1u << 4)
+#define SRC_SRMASK_TEMPSENSE_MASK       (1u << 5)
+#define SRC_SRMASK_EDGELOCK_MASK        (1u << 6)
+#define SRC_SRMASK_JTAGSW_MASK          (1u << 7)
+#define SRC_SRMASK_CM33_RESET_MASK      (1u << 8)
+#define SRC_SRMASK_CM33_LOCKUP_MASK     (1u << 9)
+#define SRC_SRMASK_CM7_RESET_MASK       (1u << 10)
+#define SRC_SRMASK_CM7_LOCKUP_MASK      (1u << 11)
+#define SRC_SRMASK_DCDC_OVVT_MASK       (1u << 12)
+#define SRC_SRMASK_ECAT_RSTO_MASK       (1u << 13)
+
+#define SRC_SRMASK_WDOG1_MASK_LOCKED       (1u << 16)
+#define SRC_SRMASK_WDOG2_MASK_LOCKED       (1u << 17)
+#define SRC_SRMASK_WDOG3_MASK_LOCKED       (1u << 18)
+#define SRC_SRMASK_WDOG4_MASK_LOCKED       (1u << 19)
+#define SRC_SRMASK_WDOG5_MASK_LOCKED       (1u << 20)
+#define SRC_SRMASK_TEMPSENSE_MASK_LOCKED   (1u << 21)
+#define SRC_SRMASK_EDGELOCK_MASK_LOCKED    (1u << 22)
+#define SRC_SRMASK_JTAGSW_MASK_LOCKED      (1u << 23)
+#define SRC_SRMASK_CM33_RESET_MASK_LOCKED  (1u << 24)
+#define SRC_SRMASK_CM33_LOCKUP_MASK_LOCKED (1u << 25)
+#define SRC_SRMASK_CM7_RESET_MASK_LOCKED   (1u << 26)
+#define SRC_SRMASK_CM7_LOCKUP_MASK_LOCKED  (1u << 27)
+#define SRC_SRMASK_DCDC_OVVT_MASK_LOCKED   (1u << 28)
+#define SRC_SRMASK_ECAT_RSTO_MASK_LOCKED   (1u << 29)
+
 /* SRC.SCR bit fields */
 
 #define SRC_SCR_BT_RELEASE_M7           (1u << 0)   /* Release M7 from boot hold */
